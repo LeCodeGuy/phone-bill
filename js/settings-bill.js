7014fc9fb4e2dc4a5245e2a3c7c77ee13ef77385
LeCodeGuy <devanj89@gmail.com>
2023-04-25 15:43:33 +0200

    added code for the settings  bill widget to function

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -1,18 +1,37 @@
 // get a reference to the sms or call radio buttons
+const billItemTypeWithSettingsElem = document.querySelector(".billItemTypeWithSettings");
 
 // get refences to all the settings fields
+const callCostSettingElem = document.querySelector(".callCostSetting");
+const smsCostSettingElem = document.querySelector(".smsCostSetting");
+const warningLevelSettingElem = document.querySelector(".warningLevelSetting");
+const criticalLevelSettingElem = document.querySelector(".criticalLevelSetting");
 
 //get a reference to the add button
+const settingsBillAddBtnElem = document.querySelector(".settingsAddBtn");
 
 //get a reference to the 'Update settings' button
+const settingsBillUpdateBtnElem = document.querySelector(".updateSettings");
 
 // create a variables that will keep track of all the settings
+var callCostSettingVal = 0;
+var smsCostSettingVal = 0;
+var warningLevelSettingVal = 0;
+var criticalLevelSettingVal = 0;
 
 // create a variables that will keep track of all three totals.
+const callTotalSettingsElem = document.querySelector(".callTotalSettings");
+const smsTotalSettingsElem = document.querySelector(".smsTotalSettings");
+const totalSettingsElem = document.querySelector(".totalSettings");
 
+var callsTotal = 0;
+var smsTotal = 0;
+var overallTotal = 0;
 //add an event listener for when the 'Update settings' button is pressed
+settingsBillUpdateBtnElem.addEventListener('click', updateBillSettings);
 
 //add an event listener for when the add button is pressed
+settingsBillAddBtnElem.addEventListener('click', billWithSettingsTotal);
 
 //in the event listener get the value from the billItemTypeRadio radio buttons
 // * add the appropriate value to the call / sms total
@@ -20,3 +39,81 @@
 // * add nothing for invalid values that is not 'call' or 'sms'.
 // * display the latest total on the screen.
 // * check the value thresholds and display the total value in the right color.
+function updateBillSettings(){
+    callCostSettingVal = callCostSettingElem.value;
+    smsCostSettingVal = smsCostSettingElem.value;
+    warningLevelSettingVal = warningLevelSettingElem.value;
+    criticalLevelSettingVal = criticalLevelSettingElem.value;
+
+    setTotalColor();
+}
+
+function billWithSettingsTotal(){    
+    // get the value entered in the billType textfield
+    var checkedRadioBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
+    
+    // update the correct total
+    if (checkedRadioBtn){
+        var billItem = checkedRadioBtn.value
+        // billItemType will be 'call' or 'sms'
+        if (billItem === "call"){
+            callsTotal += parseFloat(callCostSettingVal);//converts the string value to a numeric value before adding
+        }
+        else if (billItem === "sms"){
+            smsTotal += parseFloat(smsCostSettingVal);//converts the string value to a numeric value before adding
+        }
+        
+        //update the totals that is displayed on the screen.
+        callTotalSettingsElem.innerHTML = callsTotal.toFixed(2);
+        smsTotalSettingsElem.innerHTML = smsTotal.toFixed(2);
+        
+        overallTotal = callsTotal + smsTotal;
+        totalSettingsElem.innerHTML = overallTotal.toFixed(2);
+
+        setTotalColor();
+    } 
+}
+
+function setTotalColor (){
+    //color the total based on the criteria
+    if(criticalLevelSettingVal === 0 || warningLevelSettingVal === 0){
+        alert("Please update the settings before trying to add to the bill");
+    }
+    else if (overallTotal >= criticalLevelSettingVal){
+        // removes the warning class before adding danger class
+        totalSettingsElem.classList.remove("danger");
+        totalSettingsElem.classList.remove("warning");
+
+        // adding the danger class will make the text red
+        totalSettingsElem.classList.add("danger");
+
+        // removes the event listener from the add button as soon as critical levels are reached
+        settingsBillAddBtnElem.removeEventListener('click', billWithSettingsTotal);
+
+        // greys out the button
+        settingsBillAddBtnElem.classList.add("disableBtn");
+    }
+    else if (overallTotal >= warningLevelSettingVal){
+        // removes the danger and warning classes before adding warning class
+        totalSettingsElem.classList.remove("danger");
+        totalSettingsElem.classList.remove("warning");
+
+        // adding the warning class will make the text orange
+        totalSettingsElem.classList.add("warning");
+
+        // adds the event listener from the add button once it is below the critical level
+        settingsBillAddBtnElem.addEventListener('click', billWithSettingsTotal);
+
+        // removes the disbaleBtn class to activate the button again
+        settingsBillAddBtnElem.classList.remove("disableBtn");
+    }
+    else{
+        // removes the danger and warning classes to cater for instances where their settings
+        // are updated and do not meet the previous conditions
+        totalSettingsElem.classList.remove("danger");
+        totalSettingsElem.classList.remove("warning");
+
+        // removes the disbaleBtn class to activate the button again
+        settingsBillAddBtnElem.classList.remove("disableBtn");
+    }
+}
